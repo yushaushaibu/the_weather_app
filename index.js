b5e5a1b8d1237191abcc51251039c16fd11302b0
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-21 12:27:39 +0000

    every single key press makes request to the API

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const form = document.querySelector('form');
 const input = document.querySelector('#input');
 const btn = document.querySelector('#btn');
 
-input.addEventListener('input', () => {});
+input.addEventListener("input", (e) => {
+    getWeather(e.target.value);
+});
+
 
 // Fetch weather data
-async function getWeather() {
+async function getWeather(location) {
     const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?', {
         params: {
-            q: 'toronto,CA',
+            q: location,
             appid: '96a852a58c3f6426b65771dcefb23054',
             units: 'metric'
         }
     })
-    return response;
+    console.log(response)
 };
-
-getWeather();
